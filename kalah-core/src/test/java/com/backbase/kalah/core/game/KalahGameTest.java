e8bb7692750e0b6512f39f3025e5183552304c50
Kutlay Akyürek <kutlayakyurek@gmail.com>
2018-01-25 23:05:20 +0300

    kalah-core module initial commit

---
+++ kalah-core/src/test/java/com/backbase/kalah/core/game/KalahGameTest.java
@@ -0,0 +1,18 @@
+package com.backbase.kalah.core.game;
+
+import org.junit.Test;
+
+/**
+ * Description: Kalah game test suite
+ * Project: kalah-core
+ * Package: com.backbase.kalah.core.game
+ * Author: kakyurek
+ * Date: 2018.01.25
+ */
+public class KalahGameTest {
+
+    @Test
+    public void shouldInitGame() {
+    }
+
+}
